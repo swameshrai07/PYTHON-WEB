1e81e3820d0b16d9586b8d9e438f273123513d91
Swamesh rai <swameshrai9@gmail.com>
2025-05-15 18:17:28 +0530

    Add files via upload

---
+++ first.py
@@ -0,0 +1,7 @@
+a = int(input ("enter the first number\n"))
+b = int(input ("enter the second number\n"))
+
+print ("addition=", a+b)
+print ("subtraction=", a-b)
+print ("multiplication=", a*b)
+print ("division=", a/b)
