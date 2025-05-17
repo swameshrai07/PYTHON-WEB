0c3a0b452e4f0c7031caa169e5139cc907b76914
Swamesh rai <swameshrai9@gmail.com>
2025-05-17 11:27:46 +0530

    Add files via upload

---
+++ tupple.py
@@ -0,0 +1,8 @@
+Name = []
+Name1 = input("Enter the first movie: ")
+Name2 = input("Enter the second movie: ")
+Name3 = input("Enter the third movie: ")
+Name.append(Name1)
+Name.append(Name2)
+Name.append(Name3)
+print(Name)
