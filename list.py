eb8a8d8ebf659ff6b6995f23ebfa174631cdd50d
Swamesh rai <swameshrai9@gmail.com>
2025-05-17 11:02:41 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,8 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+extracted = numbers[0:5]
+reversed_extracted = extracted[::-1]
+print("Extracted List:", extracted)
+print("Reversed Extracted List:", reversed_extracted)
+
+
+
